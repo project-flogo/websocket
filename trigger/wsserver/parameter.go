9917fe7a1f13099bae955d9a905898dfd1f40c00
Milan M Bhagwat <mbhagwat@tibco.com>
2021-02-23 19:03:32 +0530

    Adding support for Query/Path Params & Header Parsing

---
+++ trigger/wsserver/parameter.go
@@ -0,0 +1,63 @@
+package wsserver
+
+import (
+	//"encoding/json"
+	"github.com/tibco/wi-contrib/engine/conversion"
+	"strings"
+)
+
+type Parameter struct {
+	Name      string `json:"parameterName"`
+	Type      string `json:"type"`
+	Repeating string `json:"repeating,omitempty"`
+	Required  string `json:"required,omitempty"`
+}
+
+func ParseParams(paramSchema map[string]interface{}) ([]Parameter, error) {
+
+	if paramSchema == nil {
+		return nil, nil
+	}
+
+	var parameter []Parameter
+
+	//Structure expected to be JSON schema like
+	props := paramSchema["properties"].(map[string]interface{})
+	for k, v := range props {
+		param := &Parameter{}
+		param.Name = k
+
+		//if the k is required or not
+		requiredKeys, ok := paramSchema["required"].([]interface{})
+		if ok {
+			for ran := range requiredKeys {
+				if strings.EqualFold(k, requiredKeys[ran].(string)) {
+					param.Required = "true"
+					break
+				}
+
+			}
+		}
+
+		propValue := v.(map[string]interface{})
+		for k1, v1 := range propValue {
+			if k1 == "type" {
+				if v1 != "array" {
+					param.Repeating = "false"
+				}
+				param.Type = v1.(string)
+			} else if k1 == "items" {
+				param.Repeating = "true"
+				items := v1.(map[string]interface{})
+				s, err := conversion.ConvertToString(items["type"])
+				if err != nil {
+					return nil, err
+				}
+				param.Type = s
+			}
+		}
+		parameter = append(parameter, *param)
+	}
+
+	return parameter, nil
+}
--- trigger/wsserver/trigger.go
+++ trigger/wsserver/trigger.go
@@ -5,15 +5,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
-
 	"github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
 	"github.com/project-flogo/core/action"
+	"github.com/project-flogo/core/data/coerce"
 	"github.com/project-flogo/core/data/metadata"
 	"github.com/project-flogo/core/support/log"
 	"github.com/project-flogo/core/trigger"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var triggerMd = trigger.NewMetadata(&Settings{}, &Output{}, &HandlerSettings{})
@@ -113,8 +114,7 @@ func (t *Trigger) Initialize(ctx trigger.InitContext) error {
 		method := s.Method
 		path := s.Path
 		mode := s.Mode
-
-		router.Handle(method, path, newActionHandler(t, handler, mode))
+		router.Handle(method, replacePath(path), newActionHandler(t, handler, mode))
 	}
 
 	t.logger.Debugf("Configured on port %d", t.settings.Port)
@@ -134,8 +134,15 @@ func (t *Trigger) Stop() error {
 	return t.server.Stop()
 }
 
+func replacePath(path string) string {
+	path = strings.Replace(path, "}", "", -1)
+	return strings.Replace(path, "{", ":", -1)
+}
+
 func newActionHandler(rt *Trigger, handler trigger.Handler, mode string) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+		// upgrade conn
 		rt.logger.Infof("received incomming request")
 
 		upgrader := websocket.Upgrader{}
@@ -150,6 +157,57 @@ func newActionHandler(rt *Trigger, handler trigger.Handler, mode string) httprou
 		clientAdd := conn.RemoteAddr()
 		rt.logger.Infof("Upgraded to websocket protocol")
 		rt.logger.Infof("Remote address:", clientAdd)
+
+		out := &Output{
+			QueryParams: make(map[string]interface{}),
+			PathParams:  make(map[string]string),
+			Headers:     make(map[string]interface{}),
+		}
+
+		// populate other params
+		outconfigured, err := coerce.ToObject(handler.Schemas().Output)
+		if err != nil {
+			rt.logger.Errorf("Unable to parse Output Object", err)
+			return
+		}
+
+		//PathParams
+		if len(ps) > 0 {
+			pathParamMetadata, _ := outconfigured["pathParams"]
+			if pathParamMetadata != nil {
+				resultWithPathparams, err := ParseOutputPathParams(pathParamMetadata, ps, rt)
+				if err != nil {
+					rt.logger.Info("Unable to parse Path Params: ", err)
+					//return
+				} else if resultWithPathparams != nil {
+					out.PathParams = resultWithPathparams
+				}
+			}
+		}
+		//QueryParams
+		queryParamMetadata, _ := outconfigured["queryParams"]
+		if queryParamMetadata != nil {
+			resultWithQueryparams, err := ParseOutputQueryParams(queryParamMetadata, r, rt)
+			if err != nil {
+				//return
+				rt.logger.Info("Error parsing query params: ", err)
+			} else if resultWithQueryparams != nil {
+				out.QueryParams = resultWithQueryparams
+			}
+		}
+		//Headers
+		headerMetadata, _ := outconfigured["headers"]
+		if headerMetadata != nil {
+			resultWithHeaders, err := ParseOutputHeaders(headerMetadata, r, rt)
+			if err != nil {
+				rt.logger.Info("Unable to parse Headers: ", err)
+				//return
+			} else if resultWithHeaders != nil {
+				out.Headers = resultWithHeaders
+			}
+		}
+
+		// params
 		switch mode {
 		case ModeMessage:
 			defer conn.Close()
@@ -159,16 +217,11 @@ func newActionHandler(rt *Trigger, handler trigger.Handler, mode string) httprou
 					rt.logger.Infof("error while reading websocket message: %s", err)
 					break
 				}
-				handlerRoutine(message, handler)
+				handlerRoutine(message, handler, out)
 			}
 			rt.logger.Infof("stopped listening to websocket endpoint")
 		case ModeConnection:
-			out := &Output{
-				QueryParams:  make(map[string]string),
-				PathParams:   make(map[string]string),
-				Headers:      make(map[string]string),
-				WSconnection: conn,
-			}
+			out.WSconnection = conn
 			_, err := handler.Handle(context.Background(), out)
 			if err != nil {
 				rt.logger.Errorf("Run action  failed [%s] ", err)
@@ -178,14 +231,13 @@ func newActionHandler(rt *Trigger, handler trigger.Handler, mode string) httprou
 	}
 }
 
-func handlerRoutine(message []byte, handler trigger.Handler) error {
+func handlerRoutine(message []byte, handler trigger.Handler, out *Output) error {
 	var content interface{}
 	if isJSON(message) {
 		json.NewDecoder(bytes.NewBuffer(message)).Decode(&content)
 	} else {
 		content = string(message)
 	}
-	out := &Output{}
 	out.Content = content
 	_, err := handler.Handle(context.Background(), out)
 	if err != nil {
@@ -197,4 +249,229 @@ func handlerRoutine(message []byte, handler trigger.Handler) error {
 func isJSON(str []byte) bool {
 	var js json.RawMessage
 	return json.Unmarshal(str, &js) == nil
+}
+func getValuewithType(param Parameter, sv []string) ([]interface{}, error) {
+	var values []interface{}
+	switch param.Type { // json schema data type
+	case "number":
+		if param.Repeating == "false" {
+			v, err := strconv.ParseFloat(sv[0], 64)
+			if err != nil {
+				return nil, fmt.Errorf("value %s is not a %s type", sv[0], param.Type)
+			}
+			values = append(values, v)
+		} else {
+			for _, item := range sv {
+				v, err := strconv.ParseFloat(item, 64)
+				if err != nil {
+					return nil, fmt.Errorf("value %s is not a %s type", item, param.Type)
+				}
+				values = append(values, v)
+			}
+		}
+
+	case "integer":
+		if param.Repeating == "false" {
+			v, err := strconv.ParseInt(sv[0], 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("value %s is not a %s type", sv[0], param.Type)
+			}
+			values = append(values, v)
+		} else {
+			for _, item := range sv {
+				v, err := strconv.ParseInt(item, 10, 64)
+				if err != nil {
+					return nil, fmt.Errorf("value %s is not a %s type", item, param.Type)
+				}
+				values = append(values, v)
+			}
+		}
+
+	case "boolean":
+		if param.Repeating == "false" {
+			v, err := strconv.ParseBool(sv[0])
+			if err != nil {
+				return nil, fmt.Errorf("value %s is not a %s type", sv[0], param.Type)
+			}
+			values = append(values, v)
+
+		} else {
+			for _, item := range sv {
+				v, err := strconv.ParseBool(item)
+				if err != nil {
+					return nil, fmt.Errorf("value %s is not a %s type", item, param.Type)
+				}
+				values = append(values, v)
+			}
+		}
+	case "string":
+		if param.Repeating == "false" {
+			v, err := coerce.ToString(sv[0])
+			if err != nil {
+				return nil, err
+			}
+			values = append(values, v)
+
+		} else {
+			for _, item := range sv {
+				v, err := coerce.ToString(item)
+				if err != nil {
+					return nil, err
+				}
+				values = append(values, v)
+			}
+		}
+	}
+	return values, nil
+}
+
+func ParseTillValue(outputJsonData interface{}) (map[string]interface{}, error) {
+	casted, err := coerce.ToObject(outputJsonData)
+	if err != nil {
+		return nil, err
+	}
+	castedValue := casted["value"]
+	if castedValue != nil {
+		sec, err := coerce.ToObject(castedValue)
+		if err != nil {
+			return nil, err
+		}
+		return sec, nil
+	}
+	return nil, nil
+}
+func ParseOutputPathParams(outputJsonData interface{}, ps httprouter.Params, rt *Trigger) (map[string]string, error) {
+	/*for key, val := range outputJsonData.(map[string]interface{}){
+		fmt.Println("*****key is :", key, " *****value is : ", val)
+	}*/
+	sec, err := ParseTillValue(outputJsonData)
+	if err != nil {
+		rt.logger.Info("Unable to convert table value data to object", err)
+		return nil, nil
+	}
+	if sec != nil {
+		definePathParam, _ := ParseParams(sec)
+		fmt.Println("definePathParam is : ", definePathParam)
+		if definePathParam != nil {
+			pathParams := make(map[string]string)
+			for _, qParam := range definePathParam {
+				if ps.ByName(qParam.Name) == "" && strings.EqualFold(qParam.Required, "true") {
+					errMsg := fmt.Sprintf("Required path parameter [%s] is not set", qParam.Name)
+					rt.logger.Info(errMsg)
+					//rt.logger.Error(errMsg)
+					return nil, nil
+				}
+				if ps.ByName(qParam.Name) != "" {
+					values, err := getValuewithType(qParam, []string{ps.ByName(qParam.Name)})
+					if err != nil {
+						errMsg := fmt.Sprintf("Fail to validate path parameter: %v", err)
+						rt.logger.Info(errMsg)
+						//rt.logger.Error(errMsg)
+						return nil, nil
+					}
+					pathParams[qParam.Name] = values[0].(string)
+				}
+			}
+			return pathParams, nil
+		}
+	}
+	return nil, nil
+}
+
+func ParseOutputQueryParams(outputJsonData interface{}, r *http.Request, rt *Trigger) (map[string]interface{}, error) {
+	/*for key, val := range outputJsonData.(map[string]interface{}){
+		fmt.Println("*****query params key is :", key, " *****value is : ", val)
+	}*/
+	sec, err := ParseTillValue(outputJsonData)
+	if err != nil {
+		rt.logger.Info("Unable to convert table value data to object", err)
+		return nil, nil
+	}
+	if sec != nil {
+		definedQueryParams, _ := ParseParams(sec)
+		if definedQueryParams != nil {
+			queryValues := r.URL.Query()
+			queryParams := make(map[string]interface{}, len(definedQueryParams))
+			for _, qParam := range definedQueryParams {
+				value := queryValues[qParam.Name]
+				if !notEmpty(value) && strings.EqualFold(qParam.Required, "true") {
+					errMsg := fmt.Sprintf("Required query parameter [%s] is not set", qParam.Name)
+					rt.logger.Info(errMsg)
+					return nil, nil
+				}
+
+				if notEmpty(value) {
+					values, err := getValuewithType(qParam, value)
+					if err != nil {
+						errMsg := fmt.Sprintf("Fail to validate query parameter: %v", err)
+						rt.logger.Info(errMsg)
+						return nil, nil
+					}
+					if qParam.Repeating == "false" {
+						queryParams[qParam.Name] = values[0]
+					} else {
+						queryParams[qParam.Name] = values
+					}
+					//rt.logger.Debugf("Query param: Name[%s], Value[%s]", qParam.Name, queryParams[qParam.Name])
+				}
+			}
+			return queryParams, nil
+		}
+	}
+	return nil, nil
+}
+
+func ParseOutputHeaders(outputJsonData interface{}, r *http.Request, rt *Trigger) (map[string]interface{}, error) {
+	sec, err := ParseTillValue(outputJsonData)
+	if err != nil {
+		rt.logger.Info("Unable to convert table value data to object", err)
+		return nil, nil
+	}
+	if sec != nil {
+		definedHeaderParams, _ := ParseParams(sec)
+		if definedHeaderParams != nil {
+			headers := make(map[string]interface{}, len(definedHeaderParams))
+			headerValues := r.Header
+			fmt.Println("&&&&&&&&&&& header values: ", headerValues)
+			rt.logger.Debug(headerValues)
+			for _, hParam := range definedHeaderParams {
+				value := headerValues[http.CanonicalHeaderKey(hParam.Name)]
+				if len(value) == 0 && hParam.Required == "true" {
+					errMsg := fmt.Sprintf("Required header [%s] is not set", hParam.Name)
+					rt.logger.Info(errMsg)
+					return nil, nil
+				}
+				if len(value) > 0 {
+					values, err := getValuewithType(hParam, value)
+					if err != nil {
+						errMsg := fmt.Sprintf("Fail to validate header parameter: %v", err)
+						rt.logger.Info(errMsg)
+						return nil, nil
+					}
+					if hParam.Repeating == "false" {
+						headers[hParam.Name] = values[0]
+					} else {
+						headers[hParam.Name] = values
+					}
+					//rt.logger.Debugf("Header: Name[%s], Value[%s]", hParam.Name, headers[hParam.Name])
+				}
+			}
+			return headers, nil
+		}
+	}
+	return nil, nil
+}
+
+func notEmpty(array []string) bool {
+	if len(array) > 0 {
+		if len(array) == 1 {
+			if array[0] != "" && len(array[0]) > 0 {
+				return true
+			}
+			return false
+		} else {
+			return true
+		}
+	}
+	return false
 }
