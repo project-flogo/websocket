50067f39db60404d8ac1b51498ebb0192a10fad0
Milan M Bhagwat <mbhagwat@tibco.com>
2021-03-11 20:11:01 +0530

    Updating client activity code to support header,query/pathParam

--- activity/ws/activity.go
+++ activity/ws/activity.go
@@ -1,11 +1,22 @@
 package ws
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	//"net/url"
+	"strings"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/coerce"
 	"github.com/project-flogo/core/data/metadata"
+	"github.com/project-flogo/core/support/log"
 )
 
 func init() {
@@ -17,27 +28,41 @@ var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 // New create a new websocket client
 func New(ctx activity.InitContext) (activity.Activity, error) {
 	s := &Settings{}
-	err := metadata.MapToStruct(ctx.Settings(), s, true)
-	if err != nil {
-		return nil, err
-	}
+	act := &Activity{}
+	if ctx.Settings()["format"] == nil {
+		err := metadata.MapToStruct(ctx.Settings(), s, true)
+		if err != nil {
+			return nil, err
+		}
 
-	connection, _, err := websocket.DefaultDialer.Dial(s.URI, nil)
-	if err != nil {
-		return nil, err
-	}
+		connection, _, err := websocket.DefaultDialer.Dial(s.URI, nil)
+		if err != nil {
+			return nil, err
+		}
 
-	act := &Activity{
-		settings:   s,
-		connection: connection,
+		act = &Activity{
+			settings:   s,
+			connection: connection,
+			ossVersion: true,
+		}
+
+	} else {
+		act = &Activity{
+			initsettings:  ctx.Settings(),
+			cachedClients: sync.Map{},
+			ossVersion:    false,
+		}
 	}
 	return act, nil
 }
 
 // Activity is an activity that is used to invoke a Web socket operation
 type Activity struct {
-	settings   *Settings
-	connection *websocket.Conn
+	settings      *Settings
+	connection    *websocket.Conn
+	initsettings  map[string]interface{}
+	cachedClients sync.Map
+	ossVersion    bool
 }
 
 // Metadata returns the metadata for a websocket client
@@ -47,26 +72,139 @@ func (a *Activity) Metadata() *activity.Metadata {
 
 // Eval implements api.Activity.Eval - Invokes a web socket operation
 func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
-	input := &Input{}
-	ctx.GetInputObject(input)
+	if a.ossVersion {
+		input := &Input{}
+		ctx.GetInputObject(input)
 
+		var message []byte
+		if input.Message != nil {
+			if value, ok := input.Message.(string); ok {
+				message = []byte(value)
+			} else {
+				value, err := json.Marshal(input.Message)
+				if err != nil {
+					return false, err
+				}
+				message = value
+			}
+		}
+
+		err = a.connection.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			return false, err
+		}
+
+		return true, nil
+	}
+
+	fmt.Println("******************In FE version")
+
+	fmt.Println("******************ctx", ctx)
+	// create connection
+	//var uri string
+	var isWSS bool
+	url := a.initsettings["uri"]
+	var urlstr string
+	if url != "" {
+		urlstr = url.(string) //fmt.Sprintf("%v", url)
+		isWSS = strings.HasPrefix(urlstr, "wss")
+	}
+
+	var dialer websocket.Dialer
+	if isWSS {
+		allowInsecure, err := coerce.ToBool(a.initsettings["allowInsecure"])
+		if err != nil { //TODO
+		}
+		if allowInsecure {
+			config := &tls.Config{InsecureSkipVerify: true}
+			dialer = websocket.Dialer{TLSClientConfig: config}
+		}
+	} else {
+		dialer = *websocket.DefaultDialer
+	}
+	parameters, err := GetParameter(ctx, ctx.Logger())
+	if err != nil {
+		ctx.Logger().Error(err)
+		return false, err
+	}
+	fmt.Println("****************params", parameters)
+	//populate custom headers
+	h := getHeaders(ctx, parameters)
+	//populate url with path and query params
+	builtURL := buildURI(urlstr, parameters, ctx.Logger())
+	fmt.Println("***************builtURL", builtURL)
+	connection, res, err := dialer.Dial(builtURL, h)
+	if err != nil {
+		if res != nil {
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			fmt.Println("body is :", string(body), " err is: ", err)
+			ctx.Logger().Infof("res is  %s code is %v , err is %s", res.StatusCode, string(body), err)
+		}
+		return false, err
+	}
+
+	//populate msg
 	var message []byte
-	if input.Message != nil {
-		if value, ok := input.Message.(string); ok {
+	if ctx.GetInput("message") != nil {
+		if ctx.GetInput("format") != nil && ctx.GetInput("format") == "string" {
+			value, err := coerce.ToString(ctx.GetInput("message"))
+			if err != nil {
+				//TODO
+			}
 			message = []byte(value)
 		} else {
-			value, err := json.Marshal(input.Message)
+			value, err := json.Marshal(ctx.GetInput("message"))
 			if err != nil {
 				return false, err
 			}
 			message = value
 		}
 	}
-
-	err = a.connection.WriteMessage(websocket.TextMessage, message)
+	fmt.Println(message)
+	//write to connection
+	err = connection.WriteMessage(websocket.TextMessage, message)
 	if err != nil {
 		return false, err
 	}
 
 	return true, nil
+}
+
+func buildURI(uri string, param *Parameters, log log.Logger) string {
+	if param != nil {
+		if param.PathParams != nil && len(param.PathParams) > 0 {
+			uri = BuildURI(uri, param.PathParams)
+		}
+
+		if param.QueryParams != nil && len(param.QueryParams) > 0 {
+			qp := url.Values{}
+			for _, value := range param.QueryParams {
+				qp.Add(value.Name, value.ToString(log))
+			}
+			uri = uri + "?" + qp.Encode()
+		}
+
+	}
+	return uri
+}
+
+func BuildURI(uri string, values []*TypedValue) string {
+	for _, pp := range values {
+		data, _ := coerce.ToString(pp.Value)
+		uri = strings.Replace(uri, "{"+pp.Name+"}", data, -1)
+	}
+	return uri
+}
+
+func getHeaders(ctx activity.Context, param *Parameters) http.Header {
+	header := make(http.Header)
+	if param != nil && param.Headers != nil && len(param.Headers) > 0 {
+		for _, v := range param.Headers {
+			//Any input should oeverride exist header
+			// To avoid canonicalization of header name, adding headers directly to the request header map instead of using Add/Set.
+			header[v.Name] = []string{v.ToString(ctx.Logger())}
+		}
+	}
+	return header
 }
---
+++ activity/ws/parameter.go
@@ -0,0 +1,324 @@
+package ws
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/coerce"
+	"github.com/project-flogo/core/data/schema"
+	"github.com/project-flogo/core/support/log"
+)
+
+/**
+
+Support type for query and path param
+    "headers":
+        {
+            "Content_type": "Content-Type",
+            "UserName": "Tracy Li"
+        }
+    ,
+    "path_params":
+        {
+            "petID": 1101
+        }
+    ],
+    "query_params":
+        {
+            "count": 10
+        }
+*/
+
+type Parameters struct {
+	Headers        []*TypedValue `json:"headers"`
+	PathParams     []*TypedValue `json:"pathParams"`
+	QueryParams    []*TypedValue `json:"queryParams"`
+	RequestType    string
+	ResponseType   string
+	ResponseOutput string
+}
+
+func (p *Parameters) String(log log.Logger) string {
+	v, err := json.Marshal(p)
+	if err != nil {
+		log.Errorf("Parameter object to string err %s", err.Error())
+		return ""
+	}
+	return string(v)
+}
+
+type TypedValue struct {
+	Name  string      `json:"name"`
+	Value interface{} `json:"value"`
+	Type  string      `json:"type"`
+}
+
+type Param struct {
+	Name      string
+	Type      string
+	Repeating string
+	Required  string
+}
+
+type ResponseCode struct {
+	Code         int64
+	responseBody string
+}
+
+func ParseParams(paramSchema map[string]interface{}) ([]Param, error) {
+
+	if paramSchema == nil {
+		return nil, nil
+	}
+
+	var parameter []Param
+
+	//Structure expected to be JSON schema like
+	props := paramSchema["properties"].(map[string]interface{})
+	for k, v := range props {
+		param := &Param{}
+		param.Name = k
+		propValue := v.(map[string]interface{})
+		for k1, v1 := range propValue {
+			if k1 == "required" {
+				param.Required = v1.(string)
+			} else if k1 == "type" {
+				if v1 != "array" {
+					param.Repeating = "false"
+				}
+				param.Type = v1.(string)
+			} else if k1 == "items" {
+				param.Repeating = "true"
+				items := v1.(map[string]interface{})
+				s, err := coerce.ToString(items["type"])
+				if err != nil {
+					return nil, err
+				}
+				param.Type = s
+			}
+		}
+		parameter = append(parameter, *param)
+	}
+
+	return parameter, nil
+}
+
+func (t *TypedValue) ToString(log log.Logger) string {
+	if t.Value != nil {
+		v, err := coerce.ToString(t.Value)
+		if err != nil {
+			log.Error("Typed value %+v to string error %s", t, err.Error())
+			return ""
+		}
+		return v
+	}
+	return ""
+}
+
+func GetParameter(context activity.Context, log log.Logger) (params *Parameters, err error) {
+	params = &Parameters{}
+	//Headers
+	log.Debug("Reading Request Header parameters")
+	headersMap, err := LoadJsonSchemaFromInput(context, "headers")
+	if err != nil {
+		return nil, fmt.Errorf("error loading headers input schema: %s", err.Error())
+	}
+
+	if headersMap != nil {
+		headers, err := ParseParams(headersMap)
+		if err != nil {
+			return params, err
+		}
+
+		if headers != nil {
+			inputHeaders := context.GetInput("headers").(map[string]interface{}) //input.Headers
+			var typeValuesHeaders []*TypedValue
+			for _, hParam := range headers {
+				isRequired := hParam.Required
+				paramName := hParam.Name
+				if isRequired == "true" && inputHeaders[paramName] == nil {
+					return nil, fmt.Errorf("Required header parameter [%s] is not configured.", paramName)
+				}
+				if inputHeaders[paramName] != nil {
+					if hParam.Repeating == "true" {
+						val := inputHeaders[paramName]
+						switch reflect.TypeOf(val).Kind() {
+						case reflect.Slice:
+							s := reflect.ValueOf(val)
+
+							typeValue := &TypedValue{}
+							typeValue.Name = paramName
+							typeValue.Type = hParam.Type
+
+							var multiVs []string
+							for i := 0; i < s.Len(); i++ {
+								sv, _ := coerce.ToString(s.Index(i).Interface())
+
+								multiVs = append(multiVs, sv)
+							}
+
+							typeValue.Value = strings.Join(multiVs, ",")
+							typeValuesHeaders = append(typeValuesHeaders, typeValue)
+						}
+					} else {
+						typeValue := &TypedValue{}
+						typeValue.Name = paramName
+						typeValue.Value = inputHeaders[paramName]
+						typeValue.Type = hParam.Type
+						typeValuesHeaders = append(typeValuesHeaders, typeValue)
+					}
+					params.Headers = typeValuesHeaders
+				}
+			}
+		}
+	}
+	//Query Parameters
+	log.Debug("Reading Query parameters")
+	queryParamsMap, err := LoadJsonSchemaFromInput(context, "queryParams")
+	if err != nil {
+		return nil, fmt.Errorf("error loading queryParams input schema: %s", err.Error())
+	}
+	if queryParamsMap != nil {
+		queryParams, err := ParseParams(queryParamsMap)
+		if err != nil {
+			return params, err
+		}
+
+		if queryParams != nil {
+			inputQueries := context.GetInput("queryParams").(map[string]interface{}) //input.QueryParams
+			var typeValuesQueries []*TypedValue
+			for _, qParam := range queryParams {
+				isRequired := qParam.Required
+				paramName := qParam.Name
+				if isRequired == "true" && inputQueries[paramName] == nil {
+					return nil, fmt.Errorf("Required query parameter [%s] is not configured.", paramName)
+				}
+
+				if inputQueries[paramName] != nil {
+					if qParam.Repeating == "true" {
+						val := inputQueries[paramName]
+						switch reflect.TypeOf(val).Kind() {
+						case reflect.Slice:
+							s := reflect.ValueOf(val)
+							for i := 0; i < s.Len(); i++ {
+								typeValue := &TypedValue{}
+								typeValue.Name = paramName
+								typeValue.Value = s.Index(i).Interface()
+								typeValue.Type = qParam.Type
+								typeValuesQueries = append(typeValuesQueries, typeValue)
+							}
+						}
+					} else {
+						typeValue := &TypedValue{}
+						typeValue.Name = paramName
+						typeValue.Value = inputQueries[paramName]
+						typeValue.Type = qParam.Type
+						typeValuesQueries = append(typeValuesQueries, typeValue)
+					}
+					params.QueryParams = typeValuesQueries
+				}
+			}
+		}
+
+	}
+
+	//Path parameters
+	log.Debug("Reading Path parameters")
+	pathParamsMap, err := LoadJsonSchemaFromInput(context, "pathParams")
+	if err != nil {
+		return nil, fmt.Errorf("error loading pathParams input schema: %s", err.Error())
+	}
+	if pathParamsMap != nil {
+		pathParams, err := ParseParams(pathParamsMap)
+		if err != nil {
+			return params, err
+		}
+		if pathParams != nil {
+			inputPathParams := context.GetInput("pathParams").(map[string]interface{}) //input.PathParams
+			var typeValuesPath []*TypedValue
+			for _, pParam := range pathParams {
+				paramName := pParam.Name
+				if inputPathParams[paramName] == nil {
+					return nil, fmt.Errorf("Required path parameter [%s] is not configured.", paramName)
+				}
+
+				typeValue := &TypedValue{}
+				typeValue.Name = paramName
+				typeValue.Value = inputPathParams[paramName]
+				typeValue.Type = pParam.Type
+				typeValuesPath = append(typeValuesPath, typeValue)
+				params.PathParams = typeValuesPath
+
+			}
+		}
+	}
+
+	/*requestType := input.RequestType
+	if requestType == "" {
+		requestType = "application/json"
+	}
+
+	responseType, _ := coerce.ToString(context.(*instance.TaskInst).Task().ActivityConfig().GetOutput("responseType"))
+	if responseType == "" {
+		responseType = "application/json"
+	}
+
+	responseOutput, _ := coerce.ToString(context.(*instance.TaskInst).Task().ActivityConfig().GetOutput("responseOutput"))
+	if responseOutput == "" {
+		responseOutput = "application/json"
+	}
+
+	//params.RequestType = requestType
+	params.ResponseType = responseType
+	params.ResponseOutput = responseOutput*/
+
+	return params, err
+}
+
+func LoadJsonSchemaFromInput(context activity.Context, attributeName string) (map[string]interface{}, error) {
+	var schemaModel schema.Schema
+	if sIO, ok := context.(schema.HasSchemaIO); ok {
+		schemaModel = sIO.GetInputSchema(attributeName)
+		if schemaModel != nil {
+			return coerce.ToObject(schemaModel.Value())
+		}
+	}
+	return nil, nil
+}
+
+func LoadJsonSchemaFromOutput(context activity.Context, attributeName string) (map[string]interface{}, error) {
+	var schemaModel schema.Schema
+	if sIO, ok := context.(schema.HasSchemaIO); ok {
+		schemaModel = sIO.GetOutputSchema(attributeName)
+		if schemaModel != nil {
+			return coerce.ToObject(schemaModel.Value())
+		}
+	}
+	return nil, nil
+}
+
+func ParseParameters(parameters interface{}) (*Parameters, error) {
+	parameter := &Parameters{}
+
+	switch parameters.(type) {
+	case string:
+		err := json.Unmarshal([]byte(parameters.(string)), parameter)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		b, err := json.Marshal(parameters)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(b, parameter)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+	return parameter, nil
+}
