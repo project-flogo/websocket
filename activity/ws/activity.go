a8ee057039b8c50eda685a9ca5d3322554f6cd91
Milan M Bhagwat <mbhagwat@tibco.com>
2021-03-16 17:00:57 +0530

    Adding support for reading CAcerts

--- activity/ws/activity.go
+++ activity/ws/activity.go
@@ -2,11 +2,15 @@ package ws
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 
 	//"net/url"
 	"strings"
@@ -33,7 +37,7 @@ func New(ctx activity.InitContext) (activity.Activity, error) {
 		return nil, err
 	}
 	act := &Activity{
-		settings:  s,
+		settings:      s,
 		cachedClients: sync.Map{},
 	}
 	return act, nil
@@ -74,16 +78,38 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	key := ctx.ActivityHost().Name() + "-" + ctx.Name() + "-" + builtURL + "-" + fmt.Sprintf("%v", h)
 	cachedConnection, ok := a.cachedClients.Load(key)
 	var connection *websocket.Conn
-	if !ok{
+	if !ok {
 		var dialer websocket.Dialer
 		if isWSS {
+			tlsconfig := &tls.Config{}
 			allowInsecure := a.settings.AllowInsecure
 			if allowInsecure {
-				config := &tls.Config{InsecureSkipVerify: true}
-				dialer = websocket.Dialer{TLSClientConfig: config}
+				tlsconfig.InsecureSkipVerify = true
 			} else {
-				//TODO
+				// identify if OSS
+				var cacertObj map[string]interface{}
+				if a.settings.CaCert != "" {
+					err = json.Unmarshal([]byte(a.settings.CaCert), &cacertObj)
+					if err != nil { //OSS way
+						certPool, err := getCerts(a.settings.CaCert)
+						if err != nil {
+							fmt.Printf("Error while loading client trust store - %v", err)
+							return false, err
+						}
+						tlsconfig.RootCAs = certPool
+					} else { // flogo way
+						rootCAbytes, err := decodeCerts(a.settings.CaCert, ctx.Logger())
+						if err != nil {
+							ctx.Logger().Error(err)
+							return false, err
+						}
+						certPool := x509.NewCertPool()
+						certPool.AppendCertsFromPEM(rootCAbytes)
+						tlsconfig.RootCAs = certPool
+					}
+				}
 			}
+			dialer = websocket.Dialer{TLSClientConfig: tlsconfig}
 		} else {
 			dialer = *websocket.DefaultDialer
 		}
@@ -99,11 +125,10 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 		}
 		a.cachedClients.Store(key, conn)
 		connection = conn
-	}else{
+	} else {
 		ctx.Logger().Info("Reusing connection from cache")
 		connection = cachedConnection.(*websocket.Conn)
 	}
-
 
 	//populate msg
 	if input.Message != nil {
@@ -157,4 +182,89 @@ func getHeaders(ctx activity.Context, param *Parameters) http.Header {
 		}
 	}
 	return header
+}
+
+func decodeCerts(certVal string, log log.Logger) ([]byte, error) {
+	if certVal == "" {
+		return nil, fmt.Errorf("Certificate is Empty")
+	}
+
+	//if certificate comes from fileselctor it will be base64 encoded
+	if strings.HasPrefix(certVal, "{") {
+		log.Info("Certificate received from FileSelector")
+		certObj, err := coerce.ToObject(certVal)
+		if err == nil {
+			certRealValue, ok := certObj["content"].(string)
+			log.Infof("Fetched Content from Certificate Object")
+			if !ok || certRealValue == "" {
+				return nil, fmt.Errorf("Didn't found the certificate content")
+			}
+
+			index := strings.IndexAny(certRealValue, ",")
+			if index > -1 {
+				certRealValue = certRealValue[index+1:]
+			}
+
+			return base64.StdEncoding.DecodeString(certRealValue)
+		}
+		return nil, err
+	}
+
+	//if the certificate comes from application properties need to check whether that it contains , ans encoding
+	index := strings.IndexAny(certVal, ",")
+
+	if index > -1 {
+		//some encoding is there
+		log.Debugf("Certificate received from App properties with encoding")
+		encoding := certVal[:index]
+		certRealValue := certVal[index+1:]
+
+		if strings.EqualFold(encoding, "base64") {
+			return base64.StdEncoding.DecodeString(certRealValue)
+		}
+		return nil, fmt.Errorf("Error in parsing the certificates Or we may be not be supporting the given encoding")
+	}
+
+	log.Debugf("Certificate received from App properties without encoding")
+
+	//===========These blocks of code to be removed after sriharsha fixes FLOGO-2673=================================
+	first := strings.TrimSpace(certVal[:strings.Index(certVal, "----- ")] + "-----")
+	middle := strings.TrimSpace(certVal[strings.Index(certVal, "----- ")+5 : strings.Index(certVal, " -----")])
+	strings.Replace(middle, " ", "\n", -1)
+	last := strings.TrimSpace(certVal[strings.Index(certVal, " -----"):])
+	certVal = first + "\n" + middle + "\n" + last
+	//===========These blocks of code to be removed after sriharsha fixes FLOGO-2673=================================
+
+	return []byte(certVal), nil
+}
+
+func getCerts(trustStore string) (*x509.CertPool, error) {
+	certPool := x509.NewCertPool()
+	fileInfo, err := os.Stat(trustStore)
+	if err != nil {
+		return certPool, fmt.Errorf("Truststore [%s] does not exist", trustStore)
+	}
+	switch mode := fileInfo.Mode(); {
+	case mode.IsDir():
+		break
+	case mode.IsRegular():
+		return certPool, fmt.Errorf("Truststore [%s] is not a directory.  Must be a directory containing trusted certificates in PEM format",
+			trustStore)
+	}
+	trustedCertFiles, err := ioutil.ReadDir(trustStore)
+	if err != nil || len(trustedCertFiles) == 0 {
+		return certPool, fmt.Errorf("Failed to read trusted certificates from [%s]  Must be a directory containing trusted certificates in PEM format", trustStore)
+	}
+	for _, trustCertFile := range trustedCertFiles {
+		fqfName := fmt.Sprintf("%s%c%s", trustStore, os.PathSeparator, trustCertFile.Name())
+		trustCertBytes, err := ioutil.ReadFile(fqfName)
+		if err != nil {
+			fmt.Errorf("Failed to read trusted certificate [%s] ... continueing", trustCertFile.Name())
+		}
+		certPool.AppendCertsFromPEM(trustCertBytes)
+	}
+	if len(certPool.Subjects()) < 1 {
+		return certPool, fmt.Errorf("Failed to read trusted certificates from [%s]  After processing all files in the directory no valid trusted certs were found", trustStore)
+	}
+	return certPool, nil
 }
