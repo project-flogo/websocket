1fadc0d59dceca893c65ccbed33d8c9fb48bd90f
milanbhagwat <milan.bhagwat@gmail.com>
2021-04-09 23:02:35 +0530

    Adding New Activity for writing to WS connection (#4)
    * Shifting msg handler code to Start from Initialize
    
    * Adding New Activity for Writing Data to WebSocket Connection
    
    * Removing redundant entry from settings section
    
    * Checking the casting of WSConnection
    
    Co-authored-by: Milan M Bhagwat <mbhagwat@tibco.com>

---
+++ activity/wswritedata/activity.go
@@ -0,0 +1,60 @@
+package wswritedata
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/coerce"
+)
+
+func init() {
+	activity.Register(&Activity{}, New)
+}
+
+var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
+
+// New create a new websocket client
+func New(ctx activity.InitContext) (activity.Activity, error) {
+	act := &Activity{}
+	return act, nil
+}
+
+// Activity is an activity that is used to invoke a Web socket operation
+type Activity struct{}
+
+// Metadata returns the metadata for a websocket client
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Invokes a web socket operation
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+	input := &Input{}
+	err = ctx.GetInputObject(input)
+	if err != nil {
+		return false, err
+	}
+	logger := ctx.Logger()
+	conn, ok := input.WSConnection.(*websocket.Conn)
+	if !ok {
+		return false, errors.New("Configured connection is not a WebSocket Connection")
+	}
+	if conn == nil {
+		return false, errors.New("WSConnection is non configured")
+	}
+	//populate msg
+	if input.Message != nil {
+		message, err := coerce.ToBytes(input.Message)
+		if err != nil {
+			return false, err
+		}
+		logger.Debug("writing data to websocket connection")
+		err = conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			return false, err
+		}
+	} else {
+		return false, errors.New("Message is non configured")
+	}
+	return true, nil
+}
