89e024b4966d122a692f3270301232e5f203e675
Milan M Bhagwat <mbhagwat@tibco.com>
2021-04-21 20:19:35 +0530

    Stop ticker and remove broken connection from cache for WSActivity

--- activity/ws/activity.go
+++ activity/ws/activity.go
@@ -299,7 +299,18 @@ func ping(connection *websocket.Conn, a *Activity) {
 					if err != ErrCloseSent {
 						e, ok := err.(net.Error);
 						if (!ok || !e.Temporary() ){
-							a.actLogger.Debugf("stopping ping ticker for conn: %v as received non temporary error while sending ping: %s ", connection.UnderlyingConn(), err.Error())
+							a.actLogger.Warnf("stopping ping ticker for conn: %v as received non temporary error while sending ping: %s ", connection.UnderlyingConn(), err.Error())
+							if (a.continuePing){ // remove connection from cache only if engine is not in shutting down state
+								a.cachedClients.Range(func(key, value interface{}) bool {
+									conn, ok := value.(*websocket.Conn)
+									if ok && (connection == conn){
+										a.actLogger.Warnf("Removing broken connection from cache: [%v] for key: [%s]", conn.UnderlyingConn(), key)
+										a.cachedClients.Delete(key)
+										return false
+									}
+									return true
+								})
+							}
 							return
 						}
 					}
